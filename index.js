70ea4e3e3f7f7e5fe78f647df323cf88ddbf5ad6
Talize Facó de Paula Queiroz <talize.queiroz@opus-software.com.br>
2020-12-18 15:09:42 -0300

    Inserção de prompt para instalar pwa

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+var deferredprompt;
+
 if ('serviceWorker' in navigator){
     navigator.serviceWorker.register('/sw.js');
 }else{
     console.log("não passou");
+}
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    deferredprompt = event;
+    console.log("Disparou  beforeinstallprompt");
+});
+
+function openPrompt() {
+    if (deferredprompt) {
+        deferredprompt.prompt();
+        deferredprompt.userChoice().then(choiseResult => {
+            if(choiseResult.outcome === 'dismissed'){
+                console.log("PWA não instalado");
+            }
+            else {
+                console.log("PWA a ser instalado");
+            }
+        });
+        deferredprompt = null;
+    }
 }
