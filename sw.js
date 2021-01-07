61a777485705e104b8ce30be14adeb69baaff96d
Talize Facó <talizefq@gmail.com>
2021-01-07 15:39:17 -0300

    addEventListener sw

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
 
 //Escolhe os arquivos a serem inseridos no cache
 self.addEventListener('install', function (event) {
+    console.log("Instalando service worker");
     event.waitUntil(
         caches.open('menor-pwa')
             .then(function (cache) {
                 cache.addAll([
                     '/',
                     '/index.html',
-                    '/index.js'
+                    '/index.js',
+                    '/manifest.json'
                 ])
             })
     );
@@ -20,7 +22,14 @@ self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
             .then(function (res) {
+                if(!res) {
+                    return fetch(event.request);
+                }
                 return res;
-            })
+            }).catch(e => console.log("Caiu no catch"))
     );
+});
+
+self.addEventListener('activate', function (event) {
+    console.log("Ativando service worker");
 });
